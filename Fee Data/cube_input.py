a608e015f8980c176fded1a2f05890810943bfd6
Ahmad-Raza <123093533+ahmadrazacdx@users.noreply.github.com>
2023-07-09 19:39:33 +0500

    Update cube_input.py

--- Fee Data/cube_input.py
+++ Fee Data/cube_input.py
@@ -1,15 +1,12 @@
-import math
 from dict_class import fee
 
 def cube(fee:dict):
-    values=[]
-    for value in fee.values():
-        values.append(value)
+    values=list(fee.values())
     user = input('Choose an option in Fee Details: ')
     if user.isdigit() and 1 <= int(user) <= len(values):
         index = int(user) - 1
         print('Value is :', values[index])
-        cube=math.pow(values[index],3)
+        cube=values[index]**3
         print(f'Cube of value is : {cube:.2f}')
     else:
         print('Invalid Option')
